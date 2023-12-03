6a37eb3fdfb5216548a4cc008cd312fa1d323b13
SerjMonstrX <SerjMonstrX@gmail.com>
2023-12-04 02:07:40 +0700

    Выполнено ДЗ #1 добавил путь через os

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,15 +1,18 @@
 import csv
 import psycopg2
 from password import pgsql_pass
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 conn = psycopg2.connect(host='localhost', database='north', user='postgres', password=pgsql_pass)
 try:
     with conn:
         with conn.cursor() as curr:
             tables_data = [
-                ('north_data/employees_data.csv', 'employees'),
-                ('north_data/customers_data.csv', 'customers'),
-                ('north_data/orders_data.csv', 'orders')
+                (os.path.join(current_dir, 'north_data', 'employees_data.csv'), 'employees'),
+                (os.path.join(current_dir, 'north_data', 'customers_data.csv'), 'customers'),
+                (os.path.join(current_dir, 'north_data', 'orders_data.csv'), 'orders')
             ]
             for file_path, table_name in tables_data:
                 with open(file_path, 'r', newline='', encoding='utf-8') as file:
