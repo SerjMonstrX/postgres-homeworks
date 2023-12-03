a00acb19472782378a8aa3b1e53c3b2d93c5654e
SerjMonstrX <SerjMonstrX@gmail.com>
2023-12-04 02:04:13 +0700

    Выполнено ДЗ #1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100),
+	title varchar(100) NOT NULL,
+	birth_date DATE,
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id int PRIMARY KEY,
+	company_name varchar(100),
+	fullname varchar(100)
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id int REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	rder_date DATE,
+	ship_city varchar(100)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,22 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+from password import pgsql_pass
+
+conn = psycopg2.connect(host='localhost', database='north', user='postgres', password=pgsql_pass)
+try:
+    with conn:
+        with conn.cursor() as curr:
+            tables_data = [
+                ('north_data/employees_data.csv', 'employees'),
+                ('north_data/customers_data.csv', 'customers'),
+                ('north_data/orders_data.csv', 'orders')
+            ]
+            for file_path, table_name in tables_data:
+                with open(file_path, 'r', newline='', encoding='utf-8') as file:
+                    csv_reader = csv.reader(file)
+                    next(csv_reader)  # Пропустить заголовок
+                    for row in csv_reader:
+                        curr.execute(f"INSERT INTO {table_name} VALUES ({','.join(['%s'] * len(row))})", row)
+
+finally:
+    conn.close()
